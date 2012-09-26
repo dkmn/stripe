c440e0bb006e35c7289e00f54d0b2fcb551c2a78
James Sansbury <james.sansbury@lullabot.com>
2012-09-25 23:28:51 -0400

    Refactoring javascript to allow for:
    - Multiple forms on a single page.
    - Drupal element error classes and error messages within the form.
    - Setting the pubKey from the Drupal.settings.
    - Honoring behavior context.
    - Named functions for binding.

--- stripe.js
+++ stripe.js
@@ -1,41 +1,69 @@
 (function ($) {
 
 Drupal.behaviors.stripeFormFix = {
-  attach: function (context)  {
-    $('.card-number').removeAttr('disabled').parent().removeClass('form-disabled');
-    $('.card-cvc').removeAttr('disabled').parent().removeClass('form-disabled');
+  attach: function (context, settings)  {
+    // This function must stay in here since we need context.
+    stripeFormBindHandler = function (index, value) {
+      // The identifier is a class on the card field.
+      $('.stripe-number.' + value, context)
+        .closest('form')
+        .submit(stripeSubmitHandler);
+    };
 
-    $("#stripe-admin-test").submit(function(event) {
-        var amount = $('.amount').val();
-        Stripe.createToken({
-            number: $('.card-number').val(),
-            cvc: $('.card-cvc').val(),
-            exp_month: $('.card-expiry-month').val(),
-            exp_year: $('.card-expiry-year').val()
-        }, amount, stripeResponseHandler);
-        // prevent the form from submitting with the default action
-        return false;
-    });
+    // Enable the form elements and clean up any classes.
+    $('.stripe-number, .stripe-cvc', context)
+      .removeAttr('disabled')
+      .parent()
+      .removeClass('form-disabled');
 
-    stripeResponseHandler = function(status, response) {
-      console.log(response);
-      if (response.error) {
-        //show the errors on the form
-        $("#stripe-admin-test").after($('<div class="payment-errors"></div>').html(response.error.message));
-      }
-      else {
-        var $form = $("#stripe-admin-test");
-        // token contains id, last4, and card type
-        var token = response['id'];
+    // Set the publishable key.
+    Stripe.setPublishableKey(settings.stripe.pubKey);
 
-        var form$ = $("#stripe-admin-test");
-
-        // insert the token into the form so it gets submitted to the server
-        $('input[name=stripe_token]').val(token);
-        // and submit
-        form$.get(0).submit();
-      }
-    }
+    // Now bind the submit handler to each form.
+    $.each(settings.stripe.identifiers, stripeFormBindHandler);
   }
-}
+};
+
+stripeSubmitHandler = function (eventObject) {
+  // Add a submitted class so we can find this item in the response handler.
+  $(this).addClass('stripe-submitted');
+
+  // Clear out all errors.
+  $('.error', this).removeClass('error');
+  $('.stripe-errors', this).remove();
+
+  // Create the token.
+  Stripe.createToken({
+      number: $('.stripe-number', this).val(),
+      cvc: $('.stripe-cvc', this).val(),
+      exp_month: $('.stripe-exp_month', this).val(),
+      exp_year: $('.stripe-exp_year', this).val()
+  }, stripeResponseHandler);
+
+  // Prevent the form from submitting with the default action.
+  return false;
+};
+
+stripeResponseHandler = function(status, response) {
+  console.log(response);
+  var form$ = $('.stripe-submitted');
+  if (response.error) {
+    //show the errors on the form.
+    form$.prepend('<div class="stripe-errors messages error"></div>');
+    $('.stripe-' + response.error.param, form$).addClass('error');
+    $('.stripe-errors', form$).append(Drupal.t(response.error.message));
+
+    // Clean up the submitted class.
+    form$.removeClass('stripe-submitted');
+  }
+  else {
+    // token contains id, last4, and card type
+    var token = response.id;
+    // insert the token into the form so it gets submitted to the server
+    $('input[name=stripe_token]').val(token);
+    // and submit
+    form$.submit();
+  }
+};
+
 })(jQuery);
